e1bb3f9dac7c09f2f0c556d8729ae13f58625333
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2023-04-05 09:18:36 +0700

    Create CuisineModel.java

---
+++ app/src/main/java/com/example/ourcuisine/Model/CuisineModel.java
@@ -0,0 +1,22 @@
+package com.example.ourcuisine.Model;
+
+public class CuisineModel
+{
+    private String id, name, origin, description;
+    public String getId()
+    {
+        return id;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public String getOrigin()
+    {
+        return origin;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+}
