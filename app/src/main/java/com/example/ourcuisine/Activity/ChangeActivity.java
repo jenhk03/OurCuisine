35e8f18422f4c526c9a1a26cfd705e356db05f6c
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2023-05-08 09:48:00 +0700

    Create ChangeActivity.java

---
+++ app/src/main/java/com/example/ourcuisine/Activity/ChangeActivity.java
@@ -0,0 +1,16 @@
+package com.example.ourcuisine.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ourcuisine.R;
+
+public class ChangeActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change);
+    }
+}
