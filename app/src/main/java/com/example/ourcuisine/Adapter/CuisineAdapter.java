d355b466eaac7272881350be1e63cd74b389d875
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2023-05-29 09:31:53 +0700

    Update CuisineAdapter.java

--- app/src/main/java/com/example/ourcuisine/Adapter/CuisineAdapter.java
+++ app/src/main/java/com/example/ourcuisine/Adapter/CuisineAdapter.java
@@ -1,15 +1,27 @@
 package com.example.ourcuisine.Adapter;
 
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
+import com.example.ourcuisine.API.APIRequestData;
+import com.example.ourcuisine.API.RetroServer;
+import com.example.ourcuisine.Activity.ChangeActivity;
+import com.example.ourcuisine.Activity.MainActivity;
 import com.example.ourcuisine.Model.CuisineModel;
+import com.example.ourcuisine.Model.ResponseModel;
 import com.example.ourcuisine.R;
 import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CuisineAdapter extends RecyclerView.Adapter<CuisineAdapter.VHCuisine>
 {
@@ -32,9 +44,9 @@ public class CuisineAdapter extends RecyclerView.Adapter<CuisineAdapter.VHCuisin
     {
         CuisineModel cm = listCuisine.get(position);
         holder.tvId.setText(cm.getId());
-        holder.tvName.setText(cm.getId());
-        holder.tvOrigin.setText(cm.getId());
-        holder.tvDescription.setText(cm.getId());
+        holder.tvName.setText(cm.getName());
+        holder.tvOrigin.setText(cm.getOrigin());
+        holder.tvDescription.setText(cm.getDescription());
     }
     @Override
     public int getItemCount()
@@ -51,6 +63,66 @@ public class CuisineAdapter extends RecyclerView.Adapter<CuisineAdapter.VHCuisin
             tvName = itemView.findViewById(R.id.tv_name);
             tvOrigin = itemView.findViewById(R.id.tv_origin);
             tvDescription = itemView.findViewById(R.id.tv_description);
+            itemView.setOnLongClickListener(new View.OnLongClickListener()
+            {
+                @Override
+                public boolean onLongClick(View v)
+                {
+                    AlertDialog.Builder message = new AlertDialog.Builder(ctx);
+                    message.setTitle("Perhatian");
+                    message.setMessage("Operasi apa yang akan dilakukan?");
+                    message.setCancelable(true);
+                    message.setNegativeButton("Hapus", new DialogInterface.OnClickListener()
+                            {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            delCuisine(tvId.getText().toString());
+                            dialog.dismiss();
+                        }
+                    }
+                    );
+                    message.setPositiveButton("Ubah", new DialogInterface.OnClickListener()
+                            {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            Intent move = new Intent(ctx, ChangeActivity.class);
+                            move.putExtra("xId", tvId.getText().toString());
+                            move.putExtra("xName", tvName.getText().toString());
+                            move.putExtra("xOrigin", tvOrigin.getText().toString());
+                            move.putExtra("xDescription", tvDescription.getText().toString());
+                            ctx.startActivity(move);
+                        }
+                    }
+                    );
+                    message.show();
+                    return false;
+                }
+            }
+            );
+        }
+        private void delCuisine(String idCuisine)
+        {
+            APIRequestData ard = RetroServer.connectRetrofit().create(APIRequestData.class);
+            Call<ResponseModel> process = ard.ardDelete(idCuisine);
+            process.enqueue(new Callback<ResponseModel>()
+            {
+                @Override
+                public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response)
+                {
+                    String code = response.body().getCode();
+                    String message = response.body().getMessage();
+                    Toast.makeText(ctx, "Kode: " + code + ", Pesan: " + message, Toast.LENGTH_SHORT).show();
+                    ((MainActivity) ctx).retrieveCuisine();
+                }
+                @Override
+                public void onFailure(Call<ResponseModel> call, Throwable t)
+                {
+                    Toast.makeText(ctx, "Gagal Menghubungi Server", Toast.LENGTH_SHORT).show();
+                }
+            }
+            );
         }
     }
 }
